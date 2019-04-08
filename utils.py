1664a1af9a0d8419c1d773b222f4a229254afb5e
Soumen Basu <mail.basu.s@gmail.com>
2019-04-09 02:54:37 +0530

    Fixed m bug

--- utils.py
+++ utils.py
@@ -13,15 +13,15 @@ def gen_abd_params(N, f, K=None, M=None):
     quorum_params_append = quorum_params.append
     if M is not None:
         n = M
-        for q1 in range(math.ceil((N-1)/2), n-f+1):
-            for q2 in range(math.ceil((N-1)/2), n-f+1):
+        for q1 in range(math.ceil((n-1)/2), n-f+1):
+            for q2 in range(math.ceil((n-1)/2), n-f+1):
                 if q1+q2 > n:
                     quorum_params_append((n, q1, q2))
 
     else:
         for n in range(2*f+1, N+1):
-            for q1 in range(math.ceil((N-1)/2), n-f+1):
-                for q2 in range(math.ceil((N-1)/2), n-f+1):
+            for q1 in range(math.ceil((n-1)/2), n-f+1):
+                for q2 in range(math.ceil((n-1)/2), n-f+1):
                     if q1+q2 > n:
                         quorum_params_append((n, q1, q2))
     return quorum_params
@@ -36,10 +36,10 @@ def gen_cas_params(N, f, K=None, M=None):
         m = M
         k_range = K+1 if K is not None and K<m else m
         for k in range(1, k_range):
-            for q1 in range(math.ceil((N-k)/2), m-f+1):
-                for q2 in range(math.ceil((N-k)/2), m-f+1):
-                    for q3 in range(math.ceil((N-k)/2), m-f+1):
-                        for q4 in range(math.ceil((N-k)/2), m-f+1):
+            for q1 in range(math.ceil((m-k)/2), m-f+1):
+                for q2 in range(math.ceil((m-k)/2), m-f+1):
+                    for q3 in range(math.ceil((m-k)/2), m-f+1):
+                        for q4 in range(math.ceil((m-k)/2), m-f+1):
                             if q1 + q3 > m and q1 + q4 > m and  q2 + q4 >= m + k and q4 > k:
                                 quorum_params_append([m, k, q1, q2, q3, q4])
 
@@ -47,10 +47,10 @@ def gen_cas_params(N, f, K=None, M=None):
         for m in range(f+1, N+1):
             k_range = K+1 if K is not None and K<m else m
             for k in range(1, k_range):
-                for q1 in range(math.ceil((N-k)/2), m-f+1):
-                    for q2 in range(math.ceil((N-k)/2), m-f+1):
-                        for q3 in range(math.ceil((N-k)/2), m-f+1):
-                            for q4 in range(math.ceil((N-k)/2), m-f+1):
+                for q1 in range(math.ceil((m-k)/2), m-f+1):
+                    for q2 in range(math.ceil((m-k)/2), m-f+1):
+                        for q3 in range(math.ceil((m-k)/2), m-f+1):
+                            for q4 in range(math.ceil((m-k)/2), m-f+1):
                                 if q1 + q3 > m and q1 + q4 > m and  q2 + q4 >= m + k and q4 > k:
                                     quorum_params_append([m, k, q1, q2, q3, q4])
     return quorum_params
